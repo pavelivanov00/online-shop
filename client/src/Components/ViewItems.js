585fb9cad84a14b0a8b0e142d47ee4b9b6e3c579
Pavel Ivanov <pavel.2000.vd@gmail.com>
2024-02-20 17:59:05 +0200

    added price, condition and category filters to the left side

--- client/src/Components/ViewItems.js
+++ client/src/Components/ViewItems.js
@@ -12,6 +12,7 @@ class ViewItems extends Component {
             viewItems: this.props.viewItems,
             fetchedItems: [],
             search: '',
+            showCategories: false,
         }
     }
     componentDidMount() {
@@ -27,17 +28,46 @@ class ViewItems extends Component {
         }
     };
 
+
+    handleCategoriesToggle = () => {
+        this.setState(prevState => ({ showCategories: !prevState.showCategories }));
+    };
+
+    handleCategoryPick = () => {
+        this.setState({ showCategories: false });
+    }
+
     render() {
-        const { viewItems, fetchedItems, search } = this.state;
+        const { viewItems, fetchedItems, search, itemCondition, showCategories } = this.state;
+
+        const categoryTags = [
+            'Electronics',
+            'Home and Garden',
+            'Toys',
+            'Books',
+            'Health and Beauty',
+            'Sports and Outdoors',
+            'Automotive',
+            'Pet Supplies'
+        ];
+
         return (
             viewItems &&
             <div>
                 <div className='navigation'>
+                    <div>
+                        <button
+                            className='button toggleCategoriesButton'
+                            onClick={this.handleCategoriesToggle}
+                        >
+                            Categories
+                        </button>
+                    </div>
                     <input
                         type='text'
                         onChange={event => this.setState({ search: event.target.value })}
                         className='searchbar'
-                        placeholder="Search"
+                        placeholder='Search'
                         value={search}
                     >
                     </input>
@@ -63,30 +93,85 @@ class ViewItems extends Component {
                     </div>
                 </div>
 
-                <div className='itemsContainer'>
-                    {fetchedItems.map((item, index) =>
-                        <div key={index} className='item'>
-                            <div className='adjustImage'>
-                                <img
-                                    src={item.item.imageURL ? item.item.imageURL : 'https://i.imgur.com/elj4mNd.png'}
-                                    className='itemImage'
-                                    alt='test'>
-                                </img>
+                <div className='itemsAndFiltersContainer'>
+                    <div className='leftPanel'>
+                        {showCategories ?
+                            <div className='categories'>
+                                {categoryTags.map((tag, index) =>
+                                    <button
+                                        key={index}
+                                        className='categoryFilter button'
+                                        onClick={this.handleCategoryPick}
+                                    >
+                                        {tag}
+                                    </button>
+                                )}
                             </div>
-                            <div className='itemTitle'>{item.item.title}</div>
+                            :
+                            <div className='filters'>
+                                <div className='priceFilter'>
+                                    <div className='priceFilterLabel'>Price:</div>
+                                    <div className='priceFilterContainer'>
+                                        <input type='text' className='priceLimit'></input>
+                                        <div className='priceSeparator'>-</div>
+                                        <input type='text' className='priceLimit'></input>
+                                    </div>
+                                </div><div className='conditionFilter'>
+                                    <div className='conditionFilterLabel'>Condition: </div>
+                                    <label className='conditionOption'>
+                                        <input
+                                            type='radio'
+                                            value='brand new'
+                                            checked={itemCondition === 'brand new'}
+                                            onChange={this.handleConditionChange} />
+                                        Brand new
+                                    </label>
+                                    <label className='conditionOption'>
+                                        <input
+                                            type='radio'
+                                            value='unpacked'
+                                            checked={itemCondition === 'unpacked'}
+                                            onChange={this.handleConditionChange} />
+                                        Unpacked
+                                    </label>
+                                    <label className='conditionOption'>
+                                        <input
+                                            type='radio'
+                                            value='damaged'
+                                            checked={itemCondition === 'damaged'}
+                                            onChange={this.handleConditionChange} />
+                                        Damaged
+                                    </label>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                    <div className='itemsContainer'>
+                        {fetchedItems.map((item, index) =>
+                            <div key={index} className='item'>
+                                <div className='adjustImage'>
+                                    <img
+                                        src={item.item.imageURL ? item.item.imageURL : 'https://i.imgur.com/elj4mNd.png'}
+                                        className='itemImage'
+                                        alt='test'>
+                                    </img>
+                                </div>
+                                <div className='itemTitle'>{item.item.title}</div>
 
-                            <div className='priceAndCartContainer'>
-                                <div className='itemPrice'>Price: {item.item.price}</div>
-                                <button
-                                    className='button addToCartButton'
-                                    onClick={this.handleAddToCartButtonClick}
-                                >
-                                    Add to cart
-                                </button>
+                                <div className='priceAndCartContainer'>
+                                    <div className='itemPrice'>Price: {item.item.price}</div>
+                                    <button
+                                        className='button addToCartButton'
+                                        onClick={this.handleAddToCartButtonClick}
+                                    >
+                                        Add to cart
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                    }
+                        )
+                        }
+                    </div>
+
                 </div>
             </div>
         )
